581d22ed74c665b83c1ebc8f03994205801aedfc
taeyony <taeyony@gmail.com>
2026-03-23 22:26:20 +0900

    chore: .next 임시 폴더 Git 업로드 제외

--- .next/dev/static/chunks/src_app_page_tsx_0f7ol-n._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_home_0ki3khh._.js","static/chunks/node_modules_recharts_es6_util_0p9ngk1._.js","static/chunks/node_modules_recharts_es6_state_0rrp55v._.js","static/chunks/node_modules_recharts_es6_component_0d9cnvp._.js","static/chunks/node_modules_recharts_es6_cartesian_0f5air8._.js","static/chunks/node_modules_recharts_es6_0p.v3a4._.js","static/chunks/node_modules_047uoa-._.js"],
-    source: "dynamic"
-});
